45bf9aa5fc26fe3571b734fa1c6f946acf80dc01
MisakaNA <sunyumeng420@gmail.com>
2020-03-25 04:57:13 -0700

    finish all things I can do ^O^ test case: 33/33
    past simple for loop and switch case (with nested while loop)

--- demo.cc
+++ demo.cc
@@ -12,10 +12,10 @@
 using namespace std;
 LexicalAnalyzer lexer;
 Token t;
-vector<int> inputNum;
+struct InstructionNode* covid_19 = new InstructionNode;
 map<string, int> inputVar;
 struct InstructionNode* programBody;
-
+int switchOp1 = -1;
 void parse_program();
 void parse_var_section();
 void parse_id_list();
@@ -34,248 +34,19 @@ InstructionNode * parse_condition();
 TokenType parse_relop();
 InstructionNode * parse_switch_stmt();
 InstructionNode * parse_for_stmt();
-void parse_case_list();
-void parse_case();
-void parse_default_case();
+InstructionNode * parse_case_list();
+InstructionNode * parse_case();
+InstructionNode * parse_default_case();
 void parse_inputs();
 void parse_num_list();
 
+
 struct InstructionNode * parse_generate_intermediate_representation()
 {
+    covid_19->type = NOOP;
+    covid_19->next = nullptr;
     parse_program();
     return programBody;
-    /* Sample program for demonstration purpose only
-     Replace the following with a parser that reads the program from stdin &
-     creates appropriate data structures to be executed by execute_program()
-     This is the imaginary input for the following construction:
-
-     a, b, c, d;
-     {
-         input a;
-         input b;
-         c = 10;
-
-         IF c <> a
-         {
-             output b;
-         }
-
-         IF c > 1
-         {
-             a = b + 900;
-             input d;
-             IF a > 10
-             {
-                 output d;
-             }
-         }
-
-         d = 0;
-         WHILE d < 4
-         {
-             c = a + d;
-             IF d > 1
-             {
-                 output d;
-             }
-             d = d + 1;
-         }
-     }
-     1 2 3 4 5 6
-
-     Assigning location for variable "a"*/
-    /*int address_a = next_available;
-    mem[next_available] = 0;
-    next_available++;
-
-    // Assigning location for variable "b"
-    int address_b = next_available;
-    mem[next_available] = 0;
-    next_available++;
-
-    // Assigning location for variable "c"
-    int address_c = next_available;
-    mem[next_available] = 0;
-    next_available++;
-
-    // Assigning location for variable "d"
-    int address_d = next_available;
-    mem[next_available] = 0;
-    next_available++;
-
-    // Assigning location for constant 10
-    int address_ten = next_available;
-    mem[next_available] = 10;
-    next_available++;
-
-    // Assigning location for constant 1
-    int address_one = next_available;
-    mem[next_available] = 1;
-    next_available++;
-
-    // Assigning location for constant 900
-    int address_ninehundred = next_available;
-    mem[next_available] = 900;
-    next_available++;
-
-    // Assigning location for constant 3
-    int address_three = next_available;
-    mem[next_available] = 3;
-    next_available++;
-
-    // Assigning location for constant 0
-    int address_zero = next_available;
-    mem[next_available] = 0;
-    next_available++;
-
-    // Assigning location for constant 4
-    int address_four = next_available;
-    mem[next_available] = 4;
-    next_available++;
-
-    struct InstructionNode * i1 = new InstructionNode;
-    struct InstructionNode * i2 = new InstructionNode;
-    struct InstructionNode * i3 = new InstructionNode;
-    struct InstructionNode * i4 = new InstructionNode;
-    struct InstructionNode * i5 = new InstructionNode;
-    struct InstructionNode * i6 = new InstructionNode;
-    struct InstructionNode * i7 = new InstructionNode;
-    struct InstructionNode * i8 = new InstructionNode;
-    struct InstructionNode * i9 = new InstructionNode;
-    struct InstructionNode * i10 = new InstructionNode;
-    struct InstructionNode * i11 = new InstructionNode;
-    struct InstructionNode * i12 = new InstructionNode;
-    struct InstructionNode * i13 = new InstructionNode;
-    struct InstructionNode * i14 = new InstructionNode;
-    struct InstructionNode * i15 = new InstructionNode;
-    struct InstructionNode * i16 = new InstructionNode;
-    struct InstructionNode * i17 = new InstructionNode;
-    struct InstructionNode * i18 = new InstructionNode;
-    struct InstructionNode * i19 = new InstructionNode;
-    struct InstructionNode * i20 = new InstructionNode;
-    struct InstructionNode * i21 = new InstructionNode;
-    struct InstructionNode * i22 = new InstructionNode;
-
-    i1->type = IN;                                      // input a
-    i1->input_inst.var_index = address_a;
-    i1->next = i2;
-
-    i2->type = IN;                                      // input b
-    i2->input_inst.var_index = address_b;
-    i2->next = i3;
-
-    i3->type = ASSIGN;                                  // c = 10
-    i3->assign_inst.left_hand_side_index = address_c;
-    i3->assign_inst.op = OPERATOR_NONE;
-    i3->assign_inst.operand1_index = address_ten;
-    i3->next = i4;
-
-    i4->type = CJMP;                                    // if c <> a
-    i4->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
-    i4->cjmp_inst.operand1_index = address_c;
-    i4->cjmp_inst.operand2_index = address_a;
-    i4->cjmp_inst.target = i6;                          // if not (c <> a) skip forward to NOOP
-    i4->next = i5;
-
-    i5->type = OUT;                                     // output b
-    i5->output_inst.var_index = address_b;
-    i5->next = i6;
-
-    i6->type = NOOP;                                    // NOOP after IF
-    i6->next = i7;
-
-    i7->type = CJMP;                                    // if c > 1
-    i7->cjmp_inst.condition_op = CONDITION_GREATER;
-    i7->cjmp_inst.operand1_index = address_c;
-    i7->cjmp_inst.operand2_index = address_one;
-    i7->cjmp_inst.target = i13;                         // if not (c > 1) skip forward to NOOP (way down)
-    i7->next = i8;
-
-    i8->type = ASSIGN;                                  // a = b + 900
-    i8->assign_inst.left_hand_side_index = address_a;
-    i8->assign_inst.op = OPERATOR_PLUS;
-    i8->assign_inst.operand1_index = address_b;
-    i8->assign_inst.operand2_index = address_ninehundred;
-    i8->next = i9;
-
-    i9->type = IN;                                      // input d
-    i9->input_inst.var_index = address_d;
-    i9->next = i10;
-
-    i10->type = CJMP;                                   // if a > 10
-    i10->cjmp_inst.condition_op = CONDITION_GREATER;
-    i10->cjmp_inst.operand1_index = address_a;
-    i10->cjmp_inst.operand2_index = address_ten;
-    i10->cjmp_inst.target = i12;                        // if not (a > 10) skipp forward to NOOP
-    i10->next = i11;
-
-    i11->type = OUT;                                    // output d
-    i11->output_inst.var_index = address_d;
-    i11->next = i12;
-
-    i12->type = NOOP;                                   // NOOP after inner IF
-    i12->next = i13;
-
-    i13->type = NOOP;                                   // NOOP after outer IF
-    i13->next = i14;
-
-    i14->type = ASSIGN;                                 // d = 0
-    i14->assign_inst.left_hand_side_index = address_d;
-    i14->assign_inst.op = OPERATOR_NONE;
-    i14->assign_inst.operand1_index = address_zero;
-    i14->next = i15;
-
-    i15->type = CJMP;                                   // if d < 4
-    i15->cjmp_inst.condition_op = CONDITION_LESS;
-    i15->cjmp_inst.operand1_index = address_d;
-    i15->cjmp_inst.operand2_index = address_four;
-    i15->cjmp_inst.target = i22;                        // if not (d < 4) skip whole WHILE body
-    i15->next = i16;
-
-    i16->type = ASSIGN;                                 // c = a + d
-    i16->assign_inst.left_hand_side_index = address_c;
-    i16->assign_inst.op = OPERATOR_PLUS;
-    i16->assign_inst.operand1_index = address_a;
-    i16->assign_inst.operand2_index = address_d;
-    i16->next = i17;
-
-    i17->type = CJMP;                                   // if d > 1
-    i17->cjmp_inst.condition_op = CONDITION_GREATER;
-    i17->cjmp_inst.operand1_index = address_d;
-    i17->cjmp_inst.operand2_index = address_one;
-    i17->cjmp_inst.target = i19;                        // if not (d > 1) skip body of IF
-    i17->next = i18;
-
-    i18->type = OUT;                                    // output d
-    i18->output_inst.var_index = address_d;
-    i18->next = i19;
-
-    i19->type = NOOP;                                   // NOOP after body of IF
-    i19->next = i20;
-
-    i20->type = ASSIGN;                                 // d = d + 1
-    i20->assign_inst.left_hand_side_index = address_d;
-    i20->assign_inst.op = OPERATOR_PLUS;
-    i20->assign_inst.operand1_index = address_d;
-    i20->assign_inst.operand2_index = address_one;
-    i20->next = i21;
-
-    i21->type = JMP;
-    i21->jmp_inst.target = i15;
-    i21->next = i22;
-
-    i22->type = NOOP;                                   // NOOP after body of WHILE
-    i22->next = NULL;
-
-    // Inputs
-    inputs.push_back(1);
-    inputs.push_back(2);
-    inputs.push_back(3);
-    inputs.push_back(4);
-    inputs.push_back(5);
-    inputs.push_back(6);
-
-    return i1;*/
 }
 
 Token peek(){
@@ -353,22 +124,29 @@ struct InstructionNode* parse_body(){
 struct InstructionNode* parse_stmt_list(){
     struct InstructionNode* inst = nullptr;
     struct InstructionNode* instl = nullptr;
+    TokenType debug1 = t.token_type;
+    string debug2 = t.lexeme;
     inst = parse_stmt();
 
     t = peek();
     if(t.token_type == ID || t.token_type == WHILE || t.token_type == IF || t.token_type == SWITCH
        || t.token_type == FOR || t.token_type == OUTPUT || t.token_type == INPUT){
         instl = parse_stmt_list();
-        inst->next = instl;
-    }/*else{
-        //set the tail of stmt_list to nullptr
-        inst->next = nullptr;
-    }*/
+
+        //assign instl to the tail of inst
+        struct InstructionNode* getLast = inst;
+        while(getLast->next != nullptr){
+            getLast = getLast->next;
+        }
+        getLast->next = instl;
+    }
     return inst;
 }
 
 struct InstructionNode* parse_stmt(){
-    struct InstructionNode *inst = nullptr;
+
+    struct InstructionNode* inst = nullptr;
+    struct InstructionNode* getLast;
     t = peek();
     switch(t.token_type){
         case ID:
@@ -382,6 +160,13 @@ struct InstructionNode* parse_stmt(){
             break;
         case SWITCH:
             inst = parse_switch_stmt();
+            getLast = inst;
+            while(getLast->next != nullptr){
+                getLast = getLast->next;
+            }
+
+            getLast->next = covid_19;
+
             break;
         case FOR:
             inst = parse_for_stmt();
@@ -395,6 +180,7 @@ struct InstructionNode* parse_stmt(){
         default:
             syntax_error();
     }
+
     return inst;
 }
 
@@ -406,6 +192,8 @@ struct InstructionNode* parse_assign_stmt(){
     assignInst->assign_inst.left_hand_side_index = inputVar[t.lexeme];
 
     expect(EQUAL);
+
+    //can b assigned to a NUM or an expr
     Token t1 = lexer.GetToken();
     Token t2 = peek();
     lexer.UngetToken(t1);
@@ -427,7 +215,8 @@ struct InstructionNode* parse_assign_stmt(){
         syntax_error();
     }
     expect(SEMICOLON);
-
+    //set next to null
+    assignInst->next = nullptr;
     return assignInst;
 }
 
@@ -487,6 +276,7 @@ InstructionNode* parse_output_stmt(){
     outputInst->type = OUT;
     t = expect(ID);
     outputInst->output_inst.var_index = inputVar[t.lexeme];
+    outputInst->next = nullptr;
     expect(SEMICOLON);
     return outputInst;
 }
@@ -497,6 +287,7 @@ InstructionNode* parse_input_stmt(){
     inputInst->type = IN;
     t = expect(ID);
     inputInst->input_inst.var_index = inputVar[t.lexeme];
+    inputInst->next = nullptr;
     expect(SEMICOLON);
     return inputInst;
 }
@@ -525,14 +316,12 @@ struct InstructionNode* parse_while_stmt(){
     noop->type = NOOP;
     noop->next = nullptr;
 
-    jmp->next = noop;
-
     struct InstructionNode* getLast = whileInst;
     while(getLast->next != nullptr){
         getLast = getLast->next;
     }
     getLast->next = jmp;
-
+    jmp->next = noop;
     whileInst->cjmp_inst.target = noop;
 
     return whileInst;
@@ -617,24 +406,33 @@ TokenType parse_relop(){
     }
 }
 
-InstructionNode * parse_switch_stmt(){
+struct InstructionNode * parse_switch_stmt(){
     auto* switchInst = new InstructionNode;
     expect(SWITCH);
-    //switchInst->type = JMP;
-    expect(ID);
-    // some shit
+
+    t = expect(ID);
+    switchOp1 = inputVar[t.lexeme];
     expect(LBRACE);
     //case_list
     t = peek();
     if(t.token_type == CASE){
-        parse_case_list();
+        switchInst = parse_case_list();
+
+
     } else{
         syntax_error();
     }
+
     //default_case
     t = peek();
     if(t.token_type == DEFAULT){
-        parse_default_case();
+
+        struct InstructionNode* getLast = switchInst;
+        while(getLast->next->next != nullptr){
+            getLast = getLast->next;
+        }
+        getLast->next = parse_default_case();
+
         expect(RBRACE);
     } else if(t.token_type == RBRACE){
         t = lexer.GetToken();
@@ -642,10 +440,10 @@ InstructionNode * parse_switch_stmt(){
     } else{
         syntax_error();
     }
-
+    return switchInst;
 }
 
-struct InstructionNode * parse_for_stmt(){
+struct InstructionNode* parse_for_stmt(){
     auto* forInst = new InstructionNode;
     auto* assignStmt = new InstructionNode;
     expect(FOR);
@@ -706,53 +504,97 @@ struct InstructionNode * parse_for_stmt(){
         getLast = getLast->next;
     }
     getLast->next = jmp;
-
     tempWhileInst->cjmp_inst.target = noop;
 
     forInst->next = tempWhileInst;
+
     return forInst;
 }
 
-void parse_case_list(){
+struct InstructionNode* parse_case_list(){
+    auto* caseNode = new InstructionNode;
+    struct InstructionNode* caseList = nullptr;
     //case
     t = peek();
     if(t.token_type == CASE){
-        parse_case();
+        caseNode = parse_case();
+
+        auto* jmp = new InstructionNode;
+        jmp->type = JMP;
+        jmp->jmp_inst.target = covid_19;
+        struct InstructionNode* getLast = caseNode->cjmp_inst.target;
+        while(getLast->next->next != nullptr){
+            getLast = getLast->next;
+        }
+        getLast->next = jmp;
+
     } else{
         syntax_error();
     }
     //case_list
     t = peek();
     if(t.token_type == CASE){
-        parse_case_list();
-    }
+        caseList = parse_case_list();
 
+        //add caseList to the tail of caseNode
+        struct InstructionNode* getLast = caseNode;
+        while(getLast->next->next != nullptr){
+            getLast = getLast->next;
+        }
+        getLast->next = caseList;
+    }
+    return caseNode;
 }
 
-void parse_case(){
+struct InstructionNode* parse_case(){
+    auto* caseInst = new InstructionNode;
     expect(CASE);
-    expect(NUM);
-    //some shit
+    caseInst->type = CJMP;
+    caseInst->cjmp_inst.operand1_index = switchOp1;
+    caseInst->cjmp_inst.condition_op = CONDITION_NOTEQUAL;
+    t = expect(NUM);
+
+    //add the NUM to mem
+    int index = next_available;
+    mem[next_available++] = stoi(t.lexeme);
+    //assign the num to operand2
+    caseInst->cjmp_inst.operand2_index = index;
+
     expect(COLON);
     //body
     t = peek();
     if (t.token_type == LBRACE){
-        parse_body();
+        caseInst->cjmp_inst.target = parse_body();
     } else{
         syntax_error();
     }
+
+    auto* noop = new InstructionNode;
+    noop->type = NOOP;
+    noop->next = nullptr;
+
+
+    struct InstructionNode* getLast = caseInst->cjmp_inst.target;
+    while(getLast->next != nullptr){
+        getLast = getLast->next;
+    }
+    caseInst->next = noop;
+    getLast->next = caseInst->next;
+    return caseInst;
 }
 
-void parse_default_case(){
+struct InstructionNode * parse_default_case(){
+    auto* defaultInst = new InstructionNode;
     expect(DEFAULT);
     expect(COLON);
     //body
     t = peek();
     if (t.token_type == LBRACE){
-        parse_body();
+        defaultInst = parse_body();
     } else{
         syntax_error();
     }
+    return defaultInst;
 }
 
 void parse_inputs(){
